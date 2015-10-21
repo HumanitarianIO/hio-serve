add0f59c3b0ee33213883ef22046cad46e4e16f8
Luis Capelo <luiscape@gmail.com>
2015-10-20 20:39:26 -0400

    updating route lines

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ module.exports = function (app, config) {
   //
   var gaul = {
     'year': null,
-    'admin': null
+    'admin': 'all'
   }
 
   app.get('/status', function (req, res) {
@@ -30,32 +30,47 @@ module.exports = function (app, config) {
   })
 
   app.param('gaul_admin', function (req, res, next, admin) {
-    if (admin) {
-      gaul.admin = admin
-    } else {
-      gaul.admin = 'all'
-    }
+    gaul.admin = admin
     next()
   })
 
   //
-  // Get GAUL files.
+  // GAUL endpoints.
   //
-  app.get('/gaul/:gaul_year/:gaul_admin', function (req, res, next) {
-    FetchGaulFile(gaul.year, gaul.admin, function (err, data) {
-      res.sendFile(data, function (err) {
-        if (err) {
-          console.log(err)
-          var payload = {
-            'message': 'File not found.',
-            'error': err
-          }
-          res.send(payload)
-        } else {
-          res.set('Content-Type', 'application/zip')
-          console.log('File sent:', fileName)
+  app.get('/gaul/:gaul_year', function (req, res, next) {
+    FetchGaulFile(gaul.year, gaul.admin, function (err, filename) {
+      console.log(gaul)
+      if (err) {
+        console.log(err)
+        var payload = {
+          'message': 'File not found.',
+          'error': err
         }
-      })
+        res.send(payload)
+      } else {
+        res.set('Content-Type', 'application/zip')
+        res.sendFile(filename, function (err) {
+          console.log('File sent:', filename)
+        })
+      }
+    })
+  })
+
+  app.get('/gaul/:gaul_year/:gaul_admin', function (req, res, next) {
+    FetchGaulFile(gaul.year, gaul.admin, function (err, filename) {
+      if (err) {
+        console.log(err)
+        var payload = {
+          'message': 'File not found.',
+          'error': err
+        }
+        res.send(payload)
+      } else {
+        res.set('Content-Type', 'application/zip')
+        res.sendFile(filename, function (err) {
+          console.log('File sent:', filename)
+        })
+      }
     })
   })
 
